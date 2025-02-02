df212070c6a7d6ec327b12afa134ea0e1e06e4ee
vighneshkumarjha12 <jhavighnesh27@gmail.com>
2025-02-02 17:13:22 +0530

    Updated templates and added static images

--- images/urls.py
+++ images/urls.py
@@ -2,19 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Set 'imgtopdf' as the home page
+    path('', views.imgtopdf, name='home'),  # Set 'imgtopdf' as the home page
     path('jpgtopng', views.jpgtopng, name='jpgtopng'),
     path('pngtojpg', views.pngtojpg, name='pngtojpg'),
     path('webptopng', views.webptopng, name='webptopng'),
     path('pngtowebp', views.pngtowebp, name='pngtowebp'),
     path('htu', views.htu, name='htu'),
     path('services', views.services, name='services'),
-    path('contact', views.contact, name='contact'),
+    path('contact', views.contact_view, name='contact'),
     path('blogs', views.blogs, name='blogs'),
     path('faq', views.faq, name='faq'),
     path('about', views.about, name='about'),
     path('career', views.career, name='career'),
     path('pdf_to_word', views.pdf_to_word, name='pdf_to_word'),
     path('pdf_to_ppt', views.pdf_to_ppt, name='pdf_to_ppt'),
-    path('imgtopdf', views.imgtopdf, name='imgtopdf'),
+
+     path('pdf_to_excel', views.pdf_to_excel, name='pdf_to_excel'),
 ]
